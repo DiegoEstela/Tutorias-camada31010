9858af3937798b29971d72d2ba86900da015f9e4
Diego Ezequiel Estela <die.estela@gmail.com>
2022-06-15 19:03:24 -0300

    correccion

--- Clase1/index.js
+++ Clase1/index.js
@@ -1,35 +1,32 @@
 class User {
-    constructor(name, surName, pet) {
-      this.name = name;
-      this.Surname = surName;
-      this.pets = [pet];
-      this.books = [];
-    }
-  
-    getFullName() {
-      return `User: ${this.name}, ${this.Surname}`;
-    }
-  
-    addPet(pet) {
-      this.pets.push(pet);
-    }
-    countPets() {
-      return this.pets.length;
-    }
-    Addbook(bookName, bookAutor) {
-      this.books.push({ bookName, bookAutor });
-    }
-    getBookNames() {
-      const getBook = this.books.map(function (get) {
-        return get.bookName;
-      });
-      return getBook;
-    }
+  constructor(name, surName, pet) {
+    this.name = name;
+    this.Surname = surName;
+    this.pets = [pet];
+    this.books = [];
   }
-  
 
-const user = new User("Diego", "Estela", "mono")
-user.Addbook("rayuela", "Juñio Cortazar")
-console.log(user)
+  getFullName() {
+    return `User: ${this.name}, ${this.Surname}`;
+  }
 
+  addPet(pet) {
+    this.pets.push(pet);
+  }
+  countPets() {
+    return this.pets.length;
+  }
+  Addbook(bookName, bookAutor) {
+    this.books.push({ bookName, bookAutor });
+  }
+  getBookNames() {
+    const getBook = this.books.map(function (get) {
+      return get.bookName;
+    });
+    return getBook;
+  }
+}
 
+const user = new User("Diego", "Estela", "mono");
+user.Addbook("rayuela", "Juñio Cortazar");
+console.log(user);
