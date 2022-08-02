a1402ab91e3ce9488716d3835534a038f18920a1
DiegoEstela <die.estela@gmail.com>
2022-08-02 19:41:34 -0300

    Entregable-SocketIo

---
+++ Clase12/entregable/src/main.js
@@ -0,0 +1,67 @@
+const express = require('express')
+
+const { Server: HttpServer } = require('http')
+const { Server: Socket } = require('socket.io')
+
+const ContenedorMemoria = require('../contenedores/ContenedorMemoria.js')
+const ContenedorArchivo = require('../contenedores/ContenedorArchivo.js')
+
+//--------------------------------------------
+// instancio servidor, socket y api
+
+const app = express()
+const httpServer = new HttpServer(app)
+const io = new Socket(httpServer)
+
+const productosApi = new ContenedorMemoria()
+const mensajesApi = new ContenedorArchivo('mensajes.json')
+
+//--------------------------------------------
+// configuro el socket
+
+io.on('connection', async socket => {
+    console.log('Nuevo cliente conectado!');
+
+    // carga inicial de productos
+    socket.emit('productos', productosApi.listarAll());
+
+    // actualizacion de productos
+    socket.on('update', producto => {
+        productosApi.guardar(producto)
+        io.sockets.emit('productos', productosApi.listarAll());
+    })
+
+    // carga inicial de mensajes
+    socket.emit('mensajes', await mensajesApi.listarAll());
+
+    // actualizacion de mensajes
+    socket.on('nuevoMensaje', async mensaje => {
+        mensaje.fyh = new Date().toLocaleString()
+        mensaje.likes = 0
+        await mensajesApi.guardar(mensaje)
+        io.sockets.emit('mensajes', await mensajesApi.listarAll());
+    })
+
+    socket.on("eventLike", (msgId)=>{
+        const msg = await mensajesApi.listar(msgId)
+        msg.likes ++
+        await mensajesApi.actualizar(msgId , msg)
+        socketServer.socket.emit("update", "se actualizaron los mensajes")
+    })
+});
+
+//--------------------------------------------
+// agrego middlewares
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+//--------------------------------------------
+// inicio el servidor
+
+const PORT = 8090
+const connectedServer = httpServer.listen(PORT, () => {
+    console.log(`Servidor http escuchando en el puerto ${connectedServer.address().port}`)
+})
+connectedServer.on('error', error => console.log(`Error en servidor ${error}`))
