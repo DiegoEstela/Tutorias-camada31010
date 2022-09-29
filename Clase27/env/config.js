1f5e99da44420ff18c004a028835ca4b254a5c92
DiegoEstela <die.estela@gmail.com>
2022-09-29 19:45:42 -0300

    cluster

---
+++ Clase27/env/config.js
@@ -0,0 +1,16 @@
+const dotenv = require('dotenv')
+dotenv.config({
+    path:
+      process.env.MODO === "prod" ? __dirname + "/.env" : __dirname + "/dev.env"
+})
+
+
+
+
+const config = {
+    modo: process.env.MODO,
+    puerto: process.env.PUERTO,
+    debug: process.env.DEBUG,
+}
+
+module.exports = config;
