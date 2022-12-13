610c2a0b64cc3670ff0efd5ed5787f51174a22f6
DiegoEstela <die.estela@gmail.com>
2022-12-13 17:48:14 +0100

    clase33

--- Clase33/GraphQl/src/index.js
+++ Clase33/GraphQl/src/index.js
@@ -21,3 +21,40 @@ const schema = buildSchema(`
         addClient(nombre: String, telefono: String): Cliente
     }
 `);
+
+let clientes = [];
+let counter = 1;
+
+const root = {
+  clientes: () => {
+    return clientes;
+  },
+  cliente: ({ id }) => {
+    return clientes.find((cliente) => cliente.id === id);
+  },
+  addClient: (data) => {
+    const newClient = {
+      id: counter,
+      nombre: data.nombre,
+      telefono: data.telefono,
+    };
+    clientes.push(newClient);
+    counter++;
+    return newClient;
+  },
+};
+
+const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+
+app.listen(4000, () => {
+  console.log("Server is running on port 4000");
+});
